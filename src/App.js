fcd0c5a467f2004d32fa1c61f0d523b11c810d9f
jessicashong <jessicashong12@gmail.com>
2023-05-26 12:00:39 -0500

    added skills and projects components--need to finish adding project images"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 
 function App() {
@@ -9,7 +11,10 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
+      <Projects />
       
+
     </div>
   );
 }
